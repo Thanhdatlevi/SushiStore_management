8108d7f41db1a37c4476728658eeac3702bd76fa
Mr Danh <danhnguyen01112@gmail.com>
2024-12-15 11:37:16 +0700

    employee and menu have done

--- src/routes/admin/index.route.js
+++ src/routes/admin/index.route.js
@@ -1,6 +1,19 @@
 const dashboardRoutes = require("./dashboard.route");
+const branchesRoute = require("./branches.route");
+const employeeRoute = require("./employee.route");
+const reportRoute = require("./reports.route");
+const menuRoute = require("./menu.route");
+
 
 module.exports = (app) => {
 
-    app.use('/admin',dashboardRoutes)
+    app.use('/admin/dashboard',dashboardRoutes)
+
+    app.use('/admin/branches', branchesRoute)
+
+    app.use('/admin/employees', employeeRoute)    
+
+    app.use('/admin/reports', reportRoute)    
+
+    app.use('/admin/menu', menuRoute)
 }
---
+++ src/routes/admin/reports.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const route = express.Router();
+// const employeeController = require("../../controllers/admin/employee.controller");
+const reportController = require("../../controllers/admin/report.controller");  // Import controller
+
+module.exports = route;
